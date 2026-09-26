85710eb43aa7b6c3883d668e87db2992c74ccaa3
zebiao <qq1355679@live.com>
2026-09-27 00:08:47 +0900

    chore: checkpoint user changes

--- src/main/java/com/kk24426/zbagentwf/user/chat/controller/ChatController.java
+++ src/main/java/com/kk24426/zbagentwf/user/chat/controller/ChatController.java
@@ -7,10 +7,6 @@
  */
 package com.kk24426.zbagentwf.user.chat.controller;
 
-import com.kk24426.zbagentwf.common.chat.ChatRequest;
-import com.kk24426.zbagentwf.common.chat.ChatResponse;
-import com.kk24426.zbagentwf.common.exception.AgentUnavailableException;
-import com.kk24426.zbagentwf.user.chat.service.ChatService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -20,6 +16,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kk24426.zbagentwf.common.chat.ChatRequest;
+import com.kk24426.zbagentwf.common.chat.ChatResponse;
+import com.kk24426.zbagentwf.common.exception.AgentUnavailableException;
+import com.kk24426.zbagentwf.user.chat.service.ChatService;
 
 /** 只开放已批准的 POST /api/chat，不扩展为通用 Agent 调用 API。 */
 @RestController
